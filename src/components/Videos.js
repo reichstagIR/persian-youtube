eefc8505591bb2f2a92d8fcb9150347a8fb860e7
GreatBritain <50a50z30s@gmail.com>
2022-09-08 17:39:11 +0430

    create Videos

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Grid} from "@mui/material"
+import {VideoCard , ChannelCard} from "./index";
+import {v4} from "uuid";
 
-const Videos = ({videos}) => {
-    console.log(videos)
+const Videos = ({videos , xs , md , sm , xl , lg , columnSpacing , rowSpacing}) => {
     return (
-        <Grid container spacing={2} ml={0}>
-
+        <Grid container columnSpacing={columnSpacing || "1.7rem"} rowSpacing={rowSpacing || "1.7rem"}>
+            {videos.map(item => <Grid item xs={xs || 12} sm={sm || 6} md={md || 6} lg={lg || 4} xl={xl || 3} key={v4()}>
+                    {item.id.videoId && <VideoCard video={item}/>}
+                    {item.id.channelId && <ChannelCard channel={item}/>}
+            </Grid>
+            )}
         </Grid>
     );
 };
