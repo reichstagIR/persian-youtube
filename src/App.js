18ccb49ce808c8dee0d974b492e1a4ebe85755b0
GreatBritain <50a50z30s@gmail.com>
2022-09-08 23:20:37 +0430

    change font

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import {BrowserRouter , Routes , Route , Navigate} from "react-router-dom";
 import {Navbar , Feed , VideoDetails , ChannelDetails , SearchFeed , NotFound} from "./components";
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-
-const theme = createMuiTheme({
-    typography: {
-        fontFamily: [
-            "iranyekan",
-            "sans-serif",
-        ].join(','),
-    },});
 
 const App = () => {
     return (
         <BrowserRouter>
-            <ThemeProvider theme={theme} >
-                <Navbar />
-                    <Routes>
-                        <Route path="/" element={<Feed />}/>
-                        <Route path="/video/:id" element={<VideoDetails />}/>
-                        <Route path="/channel/:id" element={<ChannelDetails />}/>
-                        <Route path="/notfound" element={<NotFound />}/>
-                        <Route path="/search/:query" element={<SearchFeed />}/>
-                        <Route path="/*" element={<Navigate to="/notfound" />}/>
-                    </Routes>
-            </ThemeProvider>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Feed />}/>
+                <Route path="/video/:id" element={<VideoDetails />}/>
+                <Route path="/channel/:id" element={<ChannelDetails />}/>
+                <Route path="/notfound" element={<NotFound />}/>
+                <Route path="/search/:query" element={<SearchFeed />}/>
+                <Route path="/*" element={<Navigate to="/notfound" />}/>
+            </Routes>
         </BrowserRouter>
     );
 };
