52dd95a7c6790a95020a873f5575b5afe7b3371e
GreatBritain <50a50z30s@gmail.com>
2022-09-08 17:37:04 +0430

    create Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,12 +3,10 @@ import {categories} from "./utils/constants";
 
 const Sidebar = ({selectedCategory , setSelectedCategory}) => {
 
-    console.log(selectedCategory)
-
     return (
         <>
             {categories.map((category =>
-                    <button key={category.name} onClick={() => setSelectedCategory(category.name)} className="category-btn" style={{display : "flex" , alignItems : "center" , backgroundColor : category.name === selectedCategory ? "#ff0000" : "#000"}}>
+                    <button key={category.name} onClick={() => {setSelectedCategory(category.name)}} className="category-btn" style={{display : "flex" , alignItems : "center" , backgroundColor : category.name === selectedCategory ? "#ff0000" : "#000"}}>
                         <span style={{marginLeft : "0.7rem", color : category.name === selectedCategory ? "#fff" : "#ff0000" , display : "flex" , alignItems : "center" , justifyContent : "center"}}>{category.icon}</span>
                         <span style={{color : "#fff"}}>{category.name}</span>
                     </button>
