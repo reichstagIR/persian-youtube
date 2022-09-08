3878b94866506339cf33d8d4239ea0299b969ba0
GreatBritain <50a50z30s@gmail.com>
2022-09-08 17:33:09 +0430

    create ContentsLoader

--- src/components/ContentsLoader.js
+++ src/components/ContentsLoader.js
@@ -1,10 +1,10 @@
 import React from "react"
 import ContentLoader from "react-content-loader"
 
-const ContentLoader = () => (
-    <ContentLoader speed={2} width={265} height={251} viewBox="0 0 265 251" backgroundColor="#f3f3f3" foregroundColor="#ecebeb">
+const ContentsLoader = (props) => (
+    <ContentLoader speed={2} width="440px" viewBox="0 0 265 251" backgroundColor="#f3f3f3" foregroundColor="#ded9d9" {...props}>
         <circle cx="126" cy="134" r="80" />
     </ContentLoader>
 )
 
-export default MyLoader
+export default ContentsLoader;
