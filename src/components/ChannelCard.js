4ea9ecf99468c5764067157a4399ba5daf086b14
GreatBritain <50a50z30s@gmail.com>
2022-09-08 17:30:45 +0430

    create ChannelCard

---
+++ src/components/ChannelCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Card, CardContent, CardMedia, Typography} from "@mui/material"
+import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
+import {Link} from "react-router-dom";
+
+const ChannelCard = ({channel , marginTop}) => {
+    return (
+        <Card sx={{backgroundColor : "transparent" , marginTop , boxShadow : "0"}}>
+            <CardContent sx={{height : "23.6rem" , display : "flex" , flexDirection : "column" , alignItems : "center" , justifyContent : "center"}}>
+                <CardMedia component="img" src={channel?.snippet?.thumbnails?.medium.url} sx={{height : "15.7rem" , width : "265px" , borderRadius : "50%"}} alt="channel" referrerPolicy="no-referrer"/>
+                <Link to={`/channel/${channel?.snippet?.channelId}`} style={{color : "#fff"}}>
+                    <Typography variant="subtitle1" mt="1rem" sx={{display : "flex" , alignItems : "center" , fontFamily : "iranyekan"}}>
+                        {channel?.snippet?.title}
+                        <CheckCircleRoundedIcon  sx={{width : "1rem" , mr : "0.3rem"}}/>
+                    </Typography>
+                </Link>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default ChannelCard;
---
+++ src/components/ContentsLoader.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const ContentLoader = () => (
+    <ContentLoader speed={2} width={265} height={251} viewBox="0 0 265 251" backgroundColor="#f3f3f3" foregroundColor="#ecebeb">
+        <circle cx="126" cy="134" r="80" />
+    </ContentLoader>
+)
+
+export default MyLoader
