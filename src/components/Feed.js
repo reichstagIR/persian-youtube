17da4eed472e7679d123cd18a54301ba3a7c2f90
GreatBritain <50a50z30s@gmail.com>
2022-09-08 17:33:35 +0430

    create Feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,5 +1,5 @@
 import React , {useState , useEffect} from 'react';
-import {Box , Stack} from "@mui/material"
+import {Box , Stack , CircularProgress ,Container} from "@mui/material"
 import {Sidebar , Videos} from "./index";
 import {fetchDataFromAPI} from "./utils/fetchDataFromAPI";
 
@@ -7,21 +7,35 @@ const Feed = () => {
 
     const [data, setData] = useState([]);
     const [selectedCategory , setSelectedCategory] = useState("تازه");
+    const [loading, setLading] = useState(true);
 
     useEffect(() => {
+        setData([]);
+        setLading(true);
         fetchDataFromAPI({q : selectedCategory , part : "snippet" , maxResults: "50" , order : "date"} , "search")
-            .then(response => setData(response.data.items));
+            .then(response => {
+                setData(response.data.items);
+                setLading(false);
+            });
     } , [selectedCategory]);
 
     return (
-        <Box sx={{height : "calc(100vh - 78px)" , backgroundColor : "#000"}}>
-            <Stack direction="row" sx={{height : {xs : "auto" , md : "calc(100vh - 78px)"}}}>
-                <Stack direction={{xs : "row" , md : "column"}} flexGrow="1" sx={{px : "0.4rem" , overflow : "auto"}}>
+        <Box sx={{ backgroundColor : "#000"}}>
+            <Stack direction={{xs : "column" , md : "row"}}>
+                <Stack direction={{xs : "row" , md : "column"}} sx={{px : "0.5rem" , overflow : "auto" , width : {xs : "auto" , md : "20%"} , height : {xs : "auto" , md : "calc(100vh - 87px)"} , borderLeft : {md : "1px solid #383232"}, my : {md : "0.3rem"}}}>
                     <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}/>
                 </Stack>
-                <Stack direction="row" flexGrow="6">
-                    <Videos videos={data}/>
-                </Stack>
+                {loading ?
+                    <Stack alignItems="center" justifyContent="center" sx={{width : {xs : "auto" , md : "80%"} , height : "calc(100vh - 87px)"}}>
+                        <CircularProgress sx={{color : "#fff"}}/>
+                    </Stack>
+                    :
+                    <Box sx={{width : {xs : "auto" , md : "80%"} , overflow : "hidden auto" , height : "calc(100vh - 87px)"}}>
+                        <Container maxWidth="xl">
+                            <Videos videos={data}/>
+                        </Container>
+                    </Box>
+                }
             </Stack>
         </Box>
     );
