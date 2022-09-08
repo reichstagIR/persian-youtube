407d3933672e94a1da35c31ec686bac1b0020e56
GreatBritain <50a50z30s@gmail.com>
2022-09-08 17:34:54 +0430

    add Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React , {useState} from 'react';
 import {logo} from "./utils/constants";
-import {Link} from "react-router-dom"
+import {Link , useNavigate} from "react-router-dom"
 import {Stack , Paper} from "@mui/material";
 import {Search} from "@mui/icons-material";
 
 const Navbar = () => {
+
+    const [inputText, setInputText] = useState("");
+
+    const navigate = useNavigate();
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        if(inputText){
+            navigate(`/search/${inputText}`);
+        }
+        setInputText("");
+    }
+
     return (
         <Stack flexDirection="row" alignItems="center" justifyContent="space-between" spacing="8" sx={{backgroundColor : "#000" , p : "1rem"}}>
-            <Paper elevation={0} component="form" sx={{display : "flex" , alignItems : "center" , justifyContent : "center" , borderRadius : "1.25rem" , p : "0.4rem"}}>
+            <Paper elevation={0} component="form" onSubmit={submitHandler} sx={{display : "flex" , alignItems : "center" , justifyContent : "center" , borderRadius : "1.25rem" , p : "0.4rem"}}>
                 <Search sx={{pl : "0.4rem" , color : "#ff0000"}}/>
-                <input placeholder="جستوجو..." value="" onChange={() => {}} className="search-bar"/>
+                <input placeholder="جستوجو..." value={inputText} onChange={(event) => setInputText(event.target.value)} className="search-bar"/>
             </Paper>
 
             <Link to="/" style={{display : "flex" , alignItems : "center" , justifyContent : "center"}}>
